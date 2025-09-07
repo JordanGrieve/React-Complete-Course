4d196d6a05ab1e6777a5e3c7fc800b61fbc907b2
Jordan Grieve <grievejordan@yahoo.com>
2025-09-07 17:46:56 +0100

    Winner and draw functionality added.

--- 02-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 02-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,38 +1,17 @@
-const initalGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSqaure, turns }) {
-  let gameBoard = initalGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
-  // const [gameBoard, setGameBoard] = useState(initalGameBoard);
-  //
-  // function handleSelectSquare(rowIndex, colIndex) {
-  //   setGameBoard((prevGameBoard) => {
-  //     const updatedBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
-  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedBoard;
-  //   });
-  //   onSelectSqaure();
-  // }
-
+export default function GameBoard({ onSelectSqaure, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSqaure(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button
+                  onClick={() => onSelectSqaure(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
