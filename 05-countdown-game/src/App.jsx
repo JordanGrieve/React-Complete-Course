a685911b79f17edc13c33315f61c7818e5701b14
Jordan Grieve <grievejordan@yahoo.com>
2025-10-07 19:57:58 +0100

    Add TimeChallenge component and ResultModal for countdown game

--- 05-countdown-game/src/App.jsx
+++ 05-countdown-game/src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimeChallange from "./components/TimeChallange.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+          <TimeChallange title="Easy" targetTime={1}/>
+          <TimeChallange title="Not easy" targetTime={5}/>
+          <TimeChallange title="Getting tough" targetTime={10}/>
+          <TimeChallange title="Pros only" targetTime={15}/>
+      </div>
     </>
   );
 }
