5a6d7d734a6056e227b87dbdb69d174419a19097
Jordan Grieve <grievejordan@yahoo.com>
2025-10-13 13:10:20 +0100

    Add Modal component for input validation in NewProject

---
+++ 06-project-management/src/components/Modal.jsx
@@ -0,0 +1,21 @@
+import { createPortal } from "react-dom";
+import { useImperativeHandle, useRef } from "react";
+
+export default function Modal({ children, buttonCaption, ref }) {
+  const dialog = useRef();
+  useImperativeHandle(ref, () => ({
+    open() {
+      dialog.current.showModal();
+    },
+  }));
+
+  return createPortal(
+    <dialog ref={dialog}>
+      {children}
+      <form method="dialog">
+        <button>{buttonCaption}</button>
+      </form>
+    </dialog>,
+    document.getElementById("modal-root"),
+  );
+}
--- 06-project-management/src/components/NewProject.jsx
+++ 06-project-management/src/components/NewProject.jsx
@@ -1,7 +1,11 @@
 import Input from "./Input.jsx";
 import { useRef } from "react";
+import Modal from "./Modal.jsx";
 
 export default function NewProject({ onAdd }) {
+  //Modal ref
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -11,7 +15,14 @@ export default function NewProject({ onAdd }) {
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
 
-    // TODO - validation
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDueDate.trim() === "" ||
+      enteredDescription.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
 
     onAdd({
       title: enteredTitle,
@@ -21,27 +32,34 @@ export default function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={handelSave}
-            className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md"
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={title} label="Title" />
-        <Input ref={description} label="Description" textarea />
-        <Input type="date" ref={dueDate} label="Due Date" />
+    <>
+      <Modal ref={modal} buttonCaption="Close">
+        <h2>Invalid Input</h2>
+        <p>Oops.. Looks like you forgot to enter a value</p>
+        <p>Please provide a valid input for each input feild</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handelSave}
+              className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
