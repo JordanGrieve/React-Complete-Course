3be413403593be26037f6128e983b2df605e4986
Jordan Grieve <grievejordan@yahoo.com>
2025-10-24 20:27:03 +0100

    Add QuestionTimer component to manage question timeouts in the quiz

--- 09-quiz-master/src/components/Quiz.jsx
+++ 09-quiz-master/src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
 import quizIsCompleteImg from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -31,6 +32,10 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer
+          timeout={10000}
+          onTimeOut={() => handelSelectAnswer(null)}
+        />
         <h2>{QUESTIONS[activeQuestionsIndex].text}</h2>
         <ul id="answers">
           {shuffleAnswers.map((answer) => (
