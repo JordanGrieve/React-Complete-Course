dc13f6fa7de3806a93febd46c95d2a2b619067e8
Jordan Grieve <grievejordan@yahoo.com>
2025-10-24 20:13:13 +0100

    Add quiz completion summary and shuffle answers functionality

--- 09-quiz-master/src/components/Quiz.jsx
+++ 09-quiz-master/src/components/Quiz.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizIsCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionsIndex = userAnswers.length;
+
+  // Is the quiz over?
+  const quizIsComplete = activeQuestionsIndex === QUESTIONS.length;
 
   function handelSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
@@ -12,12 +16,24 @@ export default function Quiz() {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizIsCompleteImg} alt="Trophy Icon" />
+        <h2>Quiz is compelte</h2>
+      </div>
+    );
+  }
+
+  const shuffleAnswers = [...QUESTIONS[activeQuestionsIndex].answers];
+  shuffleAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionsIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionsIndex].answers.map((answer) => (
+          {shuffleAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handelSelectAnswer(answer)}>
                 {answer}
