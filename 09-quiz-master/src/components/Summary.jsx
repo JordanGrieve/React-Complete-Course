4d610387040003da819538dc359c842ebab79627
Jordan Grieve <grievejordan@yahoo.com>
2025-11-01 10:06:54 +0000

    Update Summary component to calculate and display quiz result statistics

--- 09-quiz-master/src/components/Summary.jsx
+++ 09-quiz-master/src/components/Summary.jsx
@@ -2,21 +2,36 @@ import quizIsCompleteImg from "../assets/quiz-complete.png";
 import QUESTIONS from "../questions.js";
 
 const Summary = ({ userAnswers }) => {
+  const skippedAnswer = userAnswers.filter((answer) => answer === null);
+  const correctAnswers = userAnswers.filter(
+    (answer, index) => answer === QUESTIONS[index].answers[0],
+  );
+
+  const skippedAnswersShare = Math.round(
+    (skippedAnswer.length / userAnswers.length) * 100,
+  );
+
+  const correctAnswerShare = Math.round(
+    (correctAnswers.length / userAnswers.length) * 100,
+  );
+
+  const wrongAnswersShare = 100 - skippedAnswersShare - correctAnswerShare;
+
   return (
     <div id="summary">
       <img src={quizIsCompleteImg} alt="Trophy Icon" />
       <h2>Quiz is compelte</h2>
       <div id="summary-stats">
         <p>
-          <span className="number">10%</span>
-          <span className="text">Skipped%</span>
+          <span className="number">{skippedAnswersShare}%</span>
+          <span className="text">Skipped</span>
         </p>
         <p>
-          <span className="number">10%</span>
+          <span className="number">{correctAnswerShare}%</span>
           <span className="text">asnwered correctly</span>
         </p>
         <p>
-          <span className="number">10%</span>
+          <span className="number">{wrongAnswersShare}%</span>
           <span className="text">answerd incorrectly</span>
         </p>
       </div>
@@ -33,7 +48,7 @@ const Summary = ({ userAnswers }) => {
           }
 
           return (
-            <li key={answer}>
+            <li key={index}>
               <h3>{index + 1}</h3>
               <p className="question">{QUESTIONS[index].text}</p>
               <p className={cssClass}>{answer ?? "Skipped"}</p>
