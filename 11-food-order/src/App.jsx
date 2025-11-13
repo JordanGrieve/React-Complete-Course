6d783728628fb4c0bc7b1c9082082a07323fbf4a
Jordan Grieve <grievejordan@yahoo.com>
2025-11-13 21:18:44 +0000

    Add Meals component to fetch and display meal data

--- 11-food-order/src/App.jsx
+++ 11-food-order/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header.jsx";
+import Meals from "./components/Meals.jsx";
 
 function App() {
   return (
     <>
       <Header />
+      <Meals />
       <p>Stuck? Not sure how to proceed?</p>
       <p>Don't worry - we've all been there. Let's build it together!</p>
     </>
