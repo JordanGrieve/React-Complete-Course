9dca4c1506aeac6f558dc91f15178a13e4aa8b76
Jordan Grieve <grievejordan@yahoo.com>
2025-11-24 18:49:27 +0000

    Refactor Counter component to use Redux Toolkit for state management and simplify action dispatching

--- 12-redux/src/components/Counter.js
+++ 12-redux/src/components/Counter.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch, connect } from "react-redux";
-import { Component } from "react";
+import { counterActions } from "../store/index";
 
 import classes from "./Counter.module.css";
 
@@ -9,19 +9,19 @@ const Counter = () => {
   const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(5));
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
