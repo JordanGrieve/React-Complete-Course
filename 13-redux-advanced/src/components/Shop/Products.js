4dcfbe5e4afb1499e48a8108e4b320c765464dac
Jordan Grieve <grievejordan@yahoo.com>
2025-11-24 21:49:53 +0000

    Implement cart functionality with add and remove item features

--- 13-redux-advanced/src/components/Shop/Products.js
+++ 13-redux-advanced/src/components/Shop/Products.js
@@ -1,16 +1,37 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "Test",
+    description: "This is a first product - amazing!",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "Test 2",
+    description: "This is a second product - amazing!",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => {
+          return (
+            <ProductItem
+              title={product.title}
+              price={product.price}
+              id={product.id}
+              key={product.id}
+              description={product.description}
+            />
+          );
+        })}
       </ul>
     </section>
   );
--- 13-redux-advanced/src/store/cart-slice.js
+++ 13-redux-advanced/src/store/cart-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
+const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
@@ -25,6 +25,20 @@ createSlice({
       }
       state.totalQuantity++;
     },
-    removeItemFromCart(state, action) {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+      state.totalQuantity--;
+    },
   },
 });
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
