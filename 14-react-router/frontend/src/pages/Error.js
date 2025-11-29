17c92c12e7da7892d7fa327f2337360c9165b224
Jordan Grieve <grievejordan@yahoo.com>
2025-11-29 21:58:06 +0000

    Add ErrorPage component and improve error handling in EventsPage

--- 14-react-router/frontend/src/pages/Error.js
+++ 14-react-router/frontend/src/pages/Error.js
@@ -1,9 +1,29 @@
+import PageContent from "../components/PageContent";
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+
 function ErrorPage() {
+  const error = useRouteError();
+
+  let title = "An error occurred!";
+  let message = "Sorry, an unexpected error has occurred.";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found!";
+    message = "Could not find resource or page.";
+  }
+
   return (
-    <div>
-      <h1>Error 404: Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-    </div>
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
   );
 }
 
