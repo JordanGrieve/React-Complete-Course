15a06a32a399338afe9bd5754676157eefe429d2
Jordan Grieve <grievejordan@yahoo.com>
2025-11-30 11:48:18 +0000

    Add event deletion and creation functionality with improved routing in Events

--- 14-react-router/frontend/src/App.js
+++ 14-react-router/frontend/src/App.js
@@ -4,8 +4,9 @@ import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventsDetailPage, {
   loader as eventDetailLoader,
+  action as deleteEventAction,
 } from "./pages/EventsDetail";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRootLayout";
@@ -35,11 +36,12 @@ const router = createBrowserRouter([
               {
                 index: true,
                 element: <EventsDetailPage />,
+                action: deleteEventAction,
               },
               { path: "edit", element: <EditEventPage /> },
             ],
           },
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
